f7af3e7a36029cfafd98c981eba3c35b3abc99e4
Andrew Duthie <andrew@andrewduthie.com>
2017-01-19 00:24:33 -0500

    Add responsive meta tag

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 	<head>
+		<meta name="viewport" content="width=device-width">
 		<?php wp_head() ?>
 	</head>
 	<body <?php body_class(); ?>>
