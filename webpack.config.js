48011c128adc149f37c7f34b2856a2f0dc618f7f
Andrew Duthie <andrew@andrewduthie.com>
2017-04-24 22:13:10 -0400

    Remove unused NODE_ENV definition

--- webpack.config.js
+++ webpack.config.js
@@ -40,9 +40,6 @@ const config = module.exports = {
 		]
 	},
 	plugins: [
-		new webpack.DefinePlugin( {
-			'process.env.NODE_ENV': JSON.stringify( process.env.NODE_ENV )
-		} ),
 		new webpack.optimize.CommonsChunkPlugin( {
 			name: 'vendor'
 		} ),
