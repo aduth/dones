146309128b2cc1e7ece0d21874fcee0a2a443522
Andrew Duthie <andrew@andrewduthie.com>
2017-04-25 22:18:01 -0400

    Complete migration to requests middleware

--- src/state/dones/effects.js
+++ src/state/dones/effects.js
@@ -1,38 +1,57 @@
 /**
  * Internal dependencies
  */
-import Request from 'lib/request';
 import {
 	DONE_CREATE,
 	DONE_DELETE,
 	DONE_TOGGLE,
-	DONE_UPDATE
+	DONE_UPDATE,
+	REQUEST
 } from 'state/action-types';
 import { updateDone } from 'state/dones/actions';
 import { getDone } from 'state/selectors';
 
 export default {
-	[ DONE_CREATE ]: async ( store, action ) => {
+	[ DONE_CREATE ]: ( { dispatch }, action ) => {
 		const { date, text, done, transientId } = action;
-		const { id } = await Request.post( '/dones/v1/dones', {
-			body: { date, text, done }
-		} );
 
-		store.dispatch( updateDone( id, text, done, transientId ) );
+		dispatch( {
+			type: REQUEST,
+			path: '/dones/v1/dones',
+			params: {
+				method: 'POST',
+				body: { date, text, done }
+			},
+			success( { id } ) {
+				return updateDone( id, text, done, transientId );
+			}
+		} );
 	},
-	[ DONE_DELETE ]: async ( store, { id } ) => {
-		Request.delete( `/dones/v1/dones/${ id }` );
+	[ DONE_DELETE ]: ( { dispatch }, { id } ) => {
+		dispatch( {
+			type: REQUEST,
+			path: `/dones/v1/dones/${ id }`,
+			params: {
+				method: 'DELETE'
+			}
+		} );
 	},
-	[ DONE_TOGGLE ]: async ( store, { id } ) => {
-		const done = getDone( store.getState(), id );
+	[ DONE_TOGGLE ]: ( { getState, dispatch }, { id } ) => {
+		const done = getDone( getState(), id );
 		if ( done ) {
-			store.dispatch( updateDone( id, done.text, ! done.done ) );
+			dispatch( updateDone( id, done.text, ! done.done ) );
 		}
 	},
-	[ DONE_UPDATE ]: async ( store, action ) => {
+	[ DONE_UPDATE ]: ( { dispatch }, action ) => {
 		const { id, text, done } = action;
-		Request.put( `/dones/v1/dones/${ id }`, {
-			body: { text, done }
+
+		dispatch( {
+			type: REQUEST,
+			path: `/dones/v1/dones/${ id }`,
+			params: {
+				method: 'PUT',
+				body: { text, done }
+			}
 		} );
 	}
 };
