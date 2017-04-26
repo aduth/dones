bdeb739ab07d69b91b010cd2c5f64199a75fcc33
Andrew Duthie <andrew@andrewduthie.com>
2017-04-26 19:57:57 -0400

    Revert "Remove unused NODE_ENV definition"
    This reverts commit 48011c128adc149f37c7f34b2856a2f0dc618f7f.

--- webpack.config.js
+++ webpack.config.js
@@ -40,6 +40,9 @@ const config = module.exports = {
 		]
 	},
 	plugins: [
+		new webpack.DefinePlugin( {
+			'process.env.NODE_ENV': JSON.stringify( process.env.NODE_ENV )
+		} ),
 		new webpack.optimize.CommonsChunkPlugin( {
 			name: 'vendor'
 		} ),
