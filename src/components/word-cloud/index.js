4908f058e05cd96319d0ac1282de833399c758dd
Andrew Duthie <andrew@andrewduthie.com>
2017-04-27 22:24:11 -0400

    Generate tag rows by length cutoff

--- src/components/word-cloud/index.js
+++ src/components/word-cloud/index.js
@@ -2,7 +2,7 @@
  * External dependencies
  */
 import { createElement } from 'preact';
-import { mean, chunk, map } from 'lodash';
+import { mean, reduce, map, last } from 'lodash';
 
 /**
  * Internal dependencies
@@ -11,41 +11,49 @@ import Link from 'components/link';
 
 export default function( { items } ) {
 	const average = mean( map( items, 'count' ) );
+	let length;
 
 	return (
 		<ul className="word-cloud">
-			{ map( chunk( items, 4 ), ( chunked ) => {
-				return (
-					<li className="word-cloud__column">
-						{ map( chunked, ( item ) => {
-							const { text, count, url } = item;
-							const Tag = url ? Link : 'span';
+			{ map( reduce( items, ( memo, item ) => {
+				const { text, count, url } = item;
+				const Tag = url ? Link : 'span';
 
-							// Weight count against average, within reason
-							let scale = 100 + ( ( count - average ) * 20 );
-							scale = Math.max( 50, scale );
-							scale = Math.min( 250, scale );
+				// Weight count against average, within reason
+				let scale = 100 + ( ( count - average ) * 20 );
+				scale = Math.max( 50, scale );
+				scale = Math.min( 250, scale );
 
-							const props = {
-								children: text,
-								style: {
-									fontSize: scale + '%',
-								}
-							};
+				const props = {
+					children: text,
+					style: {
+						fontSize: scale + '%',
+					}
+				};
 
-							if ( url ) {
-								props.to = url;
-							}
+				if ( url ) {
+					props.to = url;
+				}
 
-							return (
-								<li key={ text } className="word-cloud__item">
-									<Tag { ...props } />
-								</li>
-							);
-						} ) }
+				if ( ! length || length > 30 ) {
+					memo.push( [] );
+					length = 0;
+				}
+
+				last( memo ).push(
+					<li key={ text } className="word-cloud__item">
+						<Tag { ...props } />
 					</li>
 				);
-			} ) }
+
+				length += text.length;
+
+				return memo;
+			}, [] ), ( column, i ) => (
+				<li key={ i } className="word-cloud__column">
+					{ column }
+				</li>
+			) ) }
 		</ul>
 	);
 }
