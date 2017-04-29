c2962b2e2ddbe635fb9d773ac8a70f36d6e92e3c
Andrew Duthie <andrew@andrewduthie.com>
2017-04-28 21:24:30 -0400

    Define viewport initial scale

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 	<head>
-		<meta name="viewport" content="width=device-width">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<?php wp_head() ?>
 	</head>
 	<body <?php body_class(); ?>>
