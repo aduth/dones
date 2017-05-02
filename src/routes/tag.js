54e4da74e5321cb7ee918836e85a2983c5cb4d03
Andrew Duthie <andrew@andrewduthie.com>
2017-05-01 21:40:13 -0400

    Wrap tag page DonesList with Card styling

--- src/routes/tag.js
+++ src/routes/tag.js
@@ -8,6 +8,7 @@ import { connect } from 'preact-redux';
  * Internal dependencies
  */
 import Page from 'components/page';
+import Card from 'components/card';
 import DonesList from 'components/dones-list';
 import { getRouteParam } from 'state/selectors';
 import { translate } from 'lib/i18n';
@@ -15,7 +16,9 @@ import { translate } from 'lib/i18n';
 function TagRoute( { tag } ) {
 	return (
 		<Page title={ translate( 'Tags' ) }>
-			<DonesList query={ { tag } } />
+			<Card>
+				<DonesList query={ { tag } } />
+			</Card>
 		</Page>
 	);
 }
