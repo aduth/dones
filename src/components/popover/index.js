fca2b3c303f5f6f8f55eb998137d96ed596be69e
Andrew Duthie <andrew@andrewduthie.com>
2017-05-01 20:54:39 -0400

    Apply forced popover position in case of exceeding bounds
    Ideally should also check on componentDidUpdate, but results in
    infinite loop of setting/unsetting forced positions. For now, we assume
    that component would only be mounted if intended to be displayed and
    calculate forced position on mount.

--- src/components/popover/index.js
+++ src/components/popover/index.js
@@ -1,26 +1,82 @@
 /**
  * External dependencies
  */
-import { createElement } from 'preact';
+import { createElement, Component } from 'preact';
 import classNames from 'classnames';
-import { map } from 'lodash';
+import { isEqual, map } from 'lodash';
 
-export default function Popover( { position, style, children } ) {
-	// Normalize position to include both y and x offsets
-	const [ y, x = 'center' ] = position.split( '-' );
-	const subPositions = [ y, x ];
+export default class Popover extends Component {
+	static defaultProps = {
+		position: 'top'
+	};
 
-	// Generate className
-	const positionClasses = map( subPositions, ( subPosition ) => `is-${ subPosition }` );
-	const classes = classNames( 'popover', positionClasses );
+	constructor() {
+		super( ...arguments );
 
-	return (
-		<div style={ style } className={ classes }>
-			{ children }
-		</div>
-	);
+		this.state = {
+			forcedPositions: []
+		};
+	}
+
+	componentDidMount() {
+		this.setForcedPositions();
+	}
+
+	setForcedPositions() {
+		const rect = this.node.getBoundingClientRect();
+		const { forcedPositions } = this.state;
+
+		const nextForcedPositions = [];
+		if ( rect.top < 0 ) {
+			nextForcedPositions[ 0 ] = 'bottom';
+		} else if ( rect.bottom > document.body.clientHeight ) {
+			nextForcedPositions[ 0 ] = 'top';
+		}
+
+		if ( rect.left < 0 ) {
+			nextForcedPositions[ 1 ] = 'right';
+		} else if ( rect.right > document.body.clientWidth ) {
+			nextForcedPositions[ 1 ] = 'left';
+		}
+
+		if ( ! isEqual( nextForcedPositions, forcedPositions ) ) {
+			this.setState( {
+				forcedPositions: nextForcedPositions
+			} );
+		}
+	}
+
+	setNode = ( node ) => {
+		this.node = node;
+	};
+
+	render() {
+		const { position, style, children } = this.props;
+		const { forcedPositions } = this.state;
+
+		// Normalize position to include both y and x offsets
+		const [ y, x = 'center' ] = position.split( '-' );
+		const subPositions = [ y, x ];
+
+		// Apply forced positions in the case that the originally rendered
+		// popover exceeds page bounds
+		if ( forcedPositions.length ) {
+			// Here we use Object.assign instead of Lodash's assign because
+			// forcedPosition is a sparse array and we need spec compliancy
+			Object.assign( subPositions, forcedPositions );
+		}
+
+		// Generate className
+		const positionClasses = map( subPositions, ( subPosition ) => `is-${ subPosition }` );
+		const classes = classNames( 'popover', positionClasses );
+
+		return (
+			<div
+				ref={ this.setNode }
+				style={ style }
+				className={ classes }>
+				{ children }
+			</div>
+		);
+	}
 }
-
-Popover.defaultProps = {
-	position: 'top'
-};
