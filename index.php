d77f6a282cd3b03a92ed5fe91cb601fa54db57e9
Andrew Duthie <andrew@andrewduthie.com>
2017-05-05 20:14:14 -0400

    Add meta charset tag

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html <?php language_attributes(); ?>>
 	<head>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<?php wp_head() ?>
 	</head>
 	<body <?php body_class(); ?>>
