76d2ef830cc4f9f489dd0f17eec3d148d97b83b4
Andrew Duthie <andrew@andrewduthie.com>
2017-05-05 20:13:15 -0400

    Specify hex sanitize callback for brand color setting

--- functions.php
+++ functions.php
@@ -178,7 +178,8 @@ function dones_customize_register( $wp_customize ) {
 	// Brand color
 
 	$wp_customize->add_setting( 'brand_color', array(
-		'default' => '#986dda'
+		'default'           => '#986dda',
+		'sanitize_callback' => 'sanitize_hex_color'
 	) );
 
 	$wp_customize->add_control( new WP_Customize_Color_Control(
