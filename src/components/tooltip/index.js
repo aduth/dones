a8f02303d7c03960d38ebd38cc2585268423a84f
Andrew Duthie <andrew@andrewduthie.com>
2017-05-11 21:43:36 -0400

    Toggle tooltip on focus events

--- src/components/tooltip/index.js
+++ src/components/tooltip/index.js
@@ -10,20 +10,27 @@ import Popover from 'components/popover';
 
 export default class Tooltip extends Component {
 	componentDidMount() {
-		this.element.parentNode.addEventListener( 'mouseenter', this.showTooltip );
-		this.element.parentNode.addEventListener( 'mouseleave', this.hideTooltip );
+		this.toggleEventBinding( true );
 	}
 
 	componentWillUnmount() {
-		this.element.parentNode.removeEventListener( 'mouseenter', this.showTooltip );
-		this.element.parentNode.removeEventListener( 'mouseleave', this.hideTooltip );
+		this.toggleEventBinding( false );
+	}
+
+	toggleEventBinding( toggle ) {
+		const bindFn = toggle ? 'addEventListener' : 'removeEventListener';
+		const { parentNode } = this.node;
+		parentNode[ bindFn ]( 'mouseenter', this.showTooltip );
+		parentNode[ bindFn ]( 'focusin', this.showTooltip );
+		parentNode[ bindFn ]( 'mouseleave', this.hideTooltip );
+		parentNode[ bindFn ]( 'focusout', this.hideTooltip );
 	}
 
 	showTooltip = () => this.setState( { isVisible: true } );
 
 	hideTooltip = () => this.setState( { isVisible: false } );
 
-	setRef = ( element ) => this.element = element;
+	setRef = ( node ) => this.node = node;
 
 	render() {
 		const { position, children } = this.props;
