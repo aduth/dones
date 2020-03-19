47c942945c4abfd48736f3cd20a632156923baa4
Andrew Duthie <andrew@andrewduthie.com>
2020-03-19 19:22:40 -0400

    Refactor HomeRoute as function component
    connect -> prsh

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,8 +1,8 @@
 /**
  * External dependencies
  */
-import { Component } from 'preact';
-import connect from 'components/connect';
+import { useEffect } from 'preact/hooks';
+import { useStore } from 'prsh';
 import { date as phpdate } from 'phpdate';
 
 /**
@@ -11,17 +11,15 @@ import { date as phpdate } from 'phpdate';
 import { replaceRoute } from 'state/routing/actions';
 import { toSiteTime } from 'lib/i18n';
 
-class HomeRoute extends Component {
-	componentWillMount() {
-		const date = phpdate( 'Y-m-d', toSiteTime() );
-		this.props.replaceRoute( `/date/${ date }/` );
-	}
+function HomeRoute() {
+	const { dispatch } = useStore();
 
-	render() {
-		return null;
-	}
+	useEffect( () => {
+		const date = phpdate( 'Y-m-d', toSiteTime() );
+		dispatch( replaceRoute( `/date/${ date }/` ) );
+	}, [] );
+
+	return null;
 }
 
-export default connect( null, {
-	replaceRoute,
-} )( HomeRoute );
+export default HomeRoute;
